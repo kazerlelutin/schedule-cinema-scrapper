e9027516eb2d0b63a68ab0ebd0f3ce827d4bb96a
Kazerlelutin <social@studiokami.fr>
2021-07-20 23:47:34 +0200

    feat(*): :tada: init repo

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:14 AS base
+
+WORKDIR /app
+
+FROM base AS dependencies
+
+COPY package*.json ./
+
+RUN npm install
+
+WORKDIR /app
+
+COPY index.js /app
+COPY .env /app
+
+USER node
+EXPOSE 3000
+
+CMD [ "npm","run","start" ]
---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const express = require("express"),
+  puppeteer = require("puppeteer"),
+  app = express(),
+  port = process.env.PORT;
+
+app.get("/schedule/:movie/:localization", async (req, res) => {
+  const { movie, localization } = req.params,
+    browser = await puppeteer.launch(),
+    page = await browser.newPage();
+  await page.goto(
+    `https://www.google.com/search?q=s%C3%A9ance+cin%C3%A9ma+${movie}+${localization}`
+  );
+
+  await page.click("#L2AGLb");
+
+  const show = await page.evaluate(() => {
+    const showtimes = document.querySelector(`[data-date]`);
+
+    if (showtimes) {
+      const parser = [];
+      showtimes.querySelectorAll("div").forEach((salle) => {
+        const divs = salle.querySelectorAll("div"),
+          show = { title: "", horaire: [] },
+          title = salle.querySelector("a");
+        if (title) {
+          show.title = title.innerText;
+        }
+        divs.forEach((el) => {
+          const txt = el.querySelector('[role="button"]');
+          if (txt) {
+            show.horaire.push({
+              type: txt.getAttribute("data-st"),
+              time: txt.querySelector("div").innerText,
+            });
+
+            parser.push(show);
+          }
+        });
+      });
+      return parser.filter((o) => o.title !== "");
+    } else {
+      return [];
+    }
+  });
+  await browser.close();
+  res.json({ show });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port : ${port}`);
+});
